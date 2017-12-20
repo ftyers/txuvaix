c30978822d11780d173f1e07717623814c941d0c
Francis Tyers <ftyers@prompsit.com>
2017-12-20 13:59:16 +0100

    somem ore stuffs

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 total=0
 win=0
 str=$1
-
+of=/tmp/chv.out
 for j in `python3 test/extract-phon.py < chv.twol | sed 's/ /_/g'`; do 
 	echo "================================================="
 	echo $j | sed 's/_/ /g' 
@@ -12,7 +12,7 @@ done
 cat test/chv.txt  | cut -f2 -d':' | hfst-proc chv.mor.hfstol > test/chv.tst
 for i in `cat test/chv.txt  | cut -f2 -d':'`; do res=`cat test/chv.txt | grep ":$i$" | cut -f1 -d':' | tr '\n' '/' | sed 's/\/$//g'`; echo "^$i/$res$"; done > test/chv.ref
 #python3 test/evaluate-morph.py test/chv.tst test/chv.ref
-
+echo "" > $of
 for i in `hfst-fst2strings chv.lexc.hfst | grep -v '<np' | grep "$str"`; do 
 	x=`echo $i | cut -f1 -d':'`; 
 	k=`echo $i | cut -f2 -d':'`; 
@@ -23,7 +23,7 @@ for i in `hfst-fst2strings chv.lexc.hfst | grep -v '<np' | grep "$str"`; do
 		r="+"
 		win=`expr $win + 1`
 	fi
-	echo -e "$r\t$x\t$k\t$z\t$y";
+	echo -e "$r\t$x\t$k\t$z\t$y" >> $of; 
 	total=`expr $total + 1`
 done
 echo "$win/$total"
@@ -32,3 +32,10 @@ d=`date`
 echo -ne "$d\t" >> test/history.log
 python3 test/evaluate-morph.py test/chv.tst test/chv.ref > test/chv.res 2>> test/history.log
 tail -4 test/history.log >/dev/stderr
+
+for i in `cat $of  | grep -P -o '(<[^>\t]+>)+' | sort -u`; do 
+	win=`cat $of | grep "$i" | grep "^+" | wc -l`; 
+	fail=`cat $of | grep "$i" | grep "^-" | wc -l`; 
+	per=`echo "$win/($win+$fail)*100" | bc -l | head -c 5`; 
+	echo -e "$per\t$win\t$fail\t$i"; 
+done | sort -gr > /tmp/chv.par
