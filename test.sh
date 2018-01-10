b9a032c3296f4fda2d28b5f9871ba89220dfcc48
Francis Tyers <ftyers@prompsit.com>
2018-01-10 18:52:57 +0100

    update

--- test.sh
+++ test.sh
@@ -15,27 +15,27 @@ echo -ne "$d\t" >> test/history.log
 python3 test/evaluate-morph.py test/chv.tst test/chv.ref > test/chv.res 2>> test/history.log
 tail -4 test/history.log >/dev/stderr
 
-echo "" > $of
-for i in `hfst-fst2strings chv.lexc.hfst | grep -v '<np' | grep "$str"`; do 
-	x=`echo $i | cut -f1 -d':'`; 
-	k=`echo $i | cut -f2 -d':'`; 
-	y=`echo $x | hfst-optimised-lookup -qp chv.gen.hfstol | cut -f2 | grep -v '^$' | sort | tr '\n' '|' | sed 's/|$//g'`
-	z=`cat test/chv.txt | grep "^$x:" | cut -f2 -d':' | sort | tr '\n' '|' | sed 's/|$//g'`;
-	r="-"
-	if [[ $y = $z ]]; then
-		r="+"
-		win=`expr $win + 1`
-	fi
-	echo -e "$r\t$x\t$k\t$z\t$y" >> $of; 
-	total=`expr $total + 1`
-done
-echo "$win/$total"
-
-for i in `cat $of  | grep -P -o '(<[^>\t]+>)+' | sort -u`; do 
-	win=`cat $of | grep "$i" | grep "^+" | wc -l`; 
-	fail=`cat $of | grep "$i" | grep "^-" | wc -l`; 
-	per=`echo "$win/($win+$fail)*100" | bc -l | head -c 5`; 
-	echo -e "$per\t$win\t$fail\t$i"; 
-done | sort -gr > /tmp/chv.par
-
-wc -l /tmp/chv.par /tmp/chv.out
+#echo "" > $of
+#for i in `hfst-fst2strings chv.lexc.hfst | grep -v '<np' | grep "$str"`; do 
+#	x=`echo $i | cut -f1 -d':'`; 
+#	k=`echo $i | cut -f2 -d':'`; 
+#	y=`echo $x | hfst-optimised-lookup -qp chv.gen.hfstol | cut -f2 | grep -v '^$' | sort | tr '\n' '|' | sed 's/|$//g'`
+#	z=`cat test/chv.txt | grep "^$x:" | cut -f2 -d':' | sort | tr '\n' '|' | sed 's/|$//g'`;
+#	r="-"
+#	if [[ $y = $z ]]; then
+#		r="+"
+#		win=`expr $win + 1`
+#	fi
+#	echo -e "$r\t$x\t$k\t$z\t$y" >> $of; 
+#	total=`expr $total + 1`
+#done
+#echo "$win/$total"
+#
+#for i in `cat $of  | grep -P -o '(<[^>\t]+>)+' | sort -u`; do 
+#	win=`cat $of | grep "$i" | grep "^+" | wc -l`; 
+#	fail=`cat $of | grep "$i" | grep "^-" | wc -l`; 
+#	per=`echo "$win/($win+$fail)*100" | bc -l | head -c 5`; 
+#	echo -e "$per\t$win\t$fail\t$i"; 
+#done | sort -gr > /tmp/chv.par
+#
+#wc -l /tmp/chv.par /tmp/chv.out
